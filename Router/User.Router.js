9de33a1f016b5c31c173400624bc17777be737eb
ritikraj07 <ritikra3rrr@gmail.com>
2024-02-22 23:24:43 +0530

    all done

--- Router/User.Router.js
+++ Router/User.Router.js
@@ -15,8 +15,8 @@ const userRouter = Router()
 userRouter.post('/create-account', async (req, res) => {
     let { email, password } = req.body
     let email_id = email
-    
-    return res.redirect(Auth({ email_id, password }));
+
+    return res.send(Auth({ email_id, password }));
 })
 
 
@@ -33,7 +33,7 @@ userRouter.post('/login', async (req, res) => {
 
     // Set the token in an HTTP-only cookie
     res.cookie('token', token, {
-        httpOnly: true,
+        // httpOnly: true,
         secure: true, // Enable this if your application uses HTTPS
         sameSite: 'strict' // Adjust as needed for your application's requirements
     });
@@ -69,7 +69,7 @@ userRouter.get('/linkedin/redirect', async (req, res) => {
             if (response.status === false) {
                 // !User has already been created
                 const errorData = encodeURIComponent(JSON.stringify(response));
-                return res.redirect(`${config.FRONTEND_URL}/login-failed?error=${errorData}`);
+                return res.redirect(`${config.FRONTEND_URL}/sign-in?error=${errorData}`);
             }
             console.log('response', response)
             const token = generateAccessToken({ user_id: response.data._id });
@@ -83,27 +83,16 @@ userRouter.get('/linkedin/redirect', async (req, res) => {
             console.log('else block', status, data);
             // Some error
             const errorData = encodeURIComponent(JSON.stringify({ status, data }));
-            return res.redirect(`${config.FRONTEND_URL}/login-failed?error=${errorData}`);
+            return res.redirect(`${config.FRONTEND_URL}/sign-up?error=${errorData}`);
         }
     } catch (error) {
         console.error('Error in LinkedIn redirect:', error);
-        return res.status(500).send('Internal Server Error');
+        
     }
 });
 
 userRouter.get('/get-user-data-form-linked', async (req, res) => {
-    res.cookie('isLogin', true, {
-        secure: true, // Enable this if your application uses HTTPS
-        sameSite: 'strict' // Adjust as needed for your application's requirements
-    });
-
-    const token = "akshfwnsaf";
-
-    res.cookie('token', token, {
-        // httpOnly: true,
-        secure: true, // Enable this if your application uses HTTPS
-        sameSite: 'strict' // Adjust as needed for your application's requirements
-    });
+    console.log('++==>', req.cookies)
 
     res.send("ok")
 })
@@ -116,7 +105,8 @@ userRouter.get('/logout', async (req, res) => {
 
 })
 
-userRouter.get('/token/:token', async (req, res) => {
+userRouter.get('/getdata', async (req, res) => {
+
 })
 
 userRouter.get('/id/:id', async (req, res) => { })
