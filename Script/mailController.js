96be8d3be27a3aed249a36270dff4d83c698742a
ritikraj07 <ritikra3rrr@gmail.com>
2024-03-18 20:03:47 +0530

    ds

--- Script/mailController.js
+++ Script/mailController.js
@@ -5,25 +5,31 @@ const path = require('path');
 
 
 const SendOTPforPasswordReset = async ({ user, otp }) => {
+    console.log('log from SendOTPforPasswordReset', user, otp)
+    try { 
+        const mail = new Mail();
+        mail.setSenderEmail(process.env.MAIL_SENDER);
+        mail.setCompanyName('LinkPod');
+        mail.setTo(user.email);
+        mail.setSubject('LinkPod - Password Reset');
 
-    const mail = new Mail();
-    mail.setSenderEmail(process.env.MAIL_SENDER);
-    mail.setCompanyName('LinkPod');
-    mail.setTo(user.email);
-    mail.setSubject('LinkPod - Password Reset');
+        const filePath = path.resolve(__dirname, '../Script/Template/passwordReset.html');
+        fs.readFile(filePath, 'utf8', async (err, html) => {
+            if (err) {
+                console.error('Error reading HTML file:', err);
+                return;
+            }
 
-    const filePath = path.resolve(__dirname, '../Script/Template/passwordReset.html');
-    fs.readFile(filePath, 'utf8', async (err, html) => {
-        if (err) {
-            console.error('Error reading HTML file:', err);
-            return;
-        }
-        
-        html = html.replace('<span id="otpPlaceholder"></span>', otp);
-        html = html.replace('<span id="user_name"></span>', user.name);
-        mail.setHTML(html)
-        mail.send();
-    })
+            html = html.replace('<span id="otpPlaceholder"></span>', otp);
+            html = html.replace('<span id="user_name"></span>', user.name);
+            mail.setHTML(html)
+            mail.send();
+        })
+    } catch (error) {
+        console.log('error from SendOTPforPasswordReset', error)
+    }
+
+   
 
 }
 
